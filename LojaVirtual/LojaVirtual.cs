4ee37492a13238f1c810a6ad441612d8aa040a66
JeffersonTales <jefferson.canalli@hotmail.com>
2024-11-30 09:52:43 -0300

    feat(auditoria): Criado aspecto de Auditoria

--- LojaVirtual/LojaVirtual.cs
+++ LojaVirtual/LojaVirtual.cs
@@ -6,15 +6,18 @@ public class LojaVirtual {
     private List<Pedido> Pedidos = [];
     
     [Notification]
+    [Auditoria]
     public void CadastrarPedido(Pedido pedido) {
         Pedidos.Add(pedido);
     }
-    
+
+    [Auditoria]
     public List<Pedido> ListarPedidos() {
         return Pedidos;
     }
     
     [Notification]
+    [Auditoria]
     public void AlterarPedidos(int id, Pedido novoPedido) {
         foreach (var pedido in Pedidos.ToList().Where(pedido => pedido.Id == id))
         {
