9bd77cb27497cce75e8cc59564a56ad9dc71b2e6
kportella <portellakaue@gmail.com>
2024-11-30 09:03:09 -0300

    classe de Pedido.cs

---
+++ LojaVirtual/Pedido.cs
@@ -0,0 +1,9 @@
+namespace LojaVirtual;
+
+public class Pedido {
+    private int Id;
+    private string Cliente;
+    private List<string> Produtos;
+    private double ValorTotal;
+    // Construtores, getters e setters...
+}
